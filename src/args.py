fb351522a17fc4b33209797cea46c3baa9733bb5
James Cullen <james.cullen@globalx.com.au>
2022-03-06 01:36:17 +1000

    feat: implement question poster

--- src/args.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Args:
-    source: str
-    destination: str
--- src/question_poster_bot.py
+++ src/question_poster_bot.py
@@ -1,31 +1,52 @@
 import praw
 import argparse
-from args import Args
 
-def run():
-    args = get_arguments()
-    print(args.source)
-    
+class QuestionPosterBot:
+	source_id: str
+	destination_id: str
+	reddit: praw.Reddit
 
-def get_arguments():
-    parser = argparse.ArgumentParser(description="Copy top-level comments from one Reddit thread to another.")
-    parser.add_argument('source', type=str, help="The thread to copy top-level comments from")
-    parser.add_argument('destination', type=str, help="The thread to submit top-level comments to")
+	def run(self):
+		self.get_arguments()
+		self.log_in()
 
-    args = parser.parse_args()
+		top_comments = self.get_top_comments()
 
-    return Args(args.source, args.destination)
+		self.post_comments(top_comments)
 
 
+	def get_arguments(self):
+		parser = argparse.ArgumentParser(description="Copy top-level comments from one Reddit thread to another.")
+		parser.add_argument('source', type=str, help="The thread to copy top-level comments from")
+		parser.add_argument('destination', type=str, help="The thread to submit top-level comments to")
 
-def log_in():
-    reddit = praw.Reddit(
-        user_agent="praw:QuestionPosterBot:0.1.0 (by /u/Zagorath)",
-    )
+		args = parser.parse_args()
 
+		self.source_id = args.source
+		self.destination_id = args.destination
 
+	def log_in(self):
+		self.reddit = praw.Reddit(
+			"questionbot",
+			user_agent="praw:QuestionPoster:1.0.0 (by /u/Zagorath)",
+		)
 
+	def get_top_comments(self):
+		source_post = self.reddit.submission(self.source_id)
+		source_post.comments.replace_more(limit=None)
+		return (comment for comment in source_post.comments if self.is_valid_comment(comment))
 
+	def post_comments(self, comments):
+		destination_post = self.reddit.submission(self.destination_id)
+
+		for comment in comments:
+			reply = f"From /u/{comment.author}:\n\n{comment.body}"
+			destination_post.reply(reply)
+
+	def is_valid_comment(self, comment: str):
+		return (comment.author is not None
+			and not comment.distinguished)
 
 if __name__ == '__main__':
-    run()
+	bot = QuestionPosterBot()
+	bot.run()
